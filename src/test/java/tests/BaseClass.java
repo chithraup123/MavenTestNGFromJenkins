bfb4a5b89520d08171d8e9a076fdce1a3643629f
chithraup123 <chithra.devikripa@gmail.com>
2022-03-21 07:05:18 +0530

    [master] Add extend report to test cases

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -2,6 +2,7 @@ package tests;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -9,14 +10,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 
 public class BaseClass {
 
 	public static WebDriver driver;
+	public static ExtentReports extReport;
+	public static ExtentTest extTest;
 
 	XSSFWorkbook wbook;
 	XSSFSheet valid_data_sheet;
@@ -24,14 +29,19 @@ public class BaseClass {
 
 	@BeforeTest
 	public void dataSetup() throws IOException {
+		System.out.println("Inside before test of BaseClass");
 		FileInputStream input = new FileInputStream("dataExcel.xlsx");
 		wbook = new XSSFWorkbook(input);
 		valid_data_sheet = wbook.getSheet("valid-data");
 		invalid_data_sheet = wbook.getSheet("invalid-data");
+
+		extReport = new ExtentReports("extent-report.html");
 	}
 
 	@BeforeMethod
-	public void setUp() {
+	public void setUp(Method method) {
+		extTest = extReport.startTest(method.getName());
+		System.out.println("Inside before method of BaseClass");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com");
@@ -41,10 +51,17 @@ public class BaseClass {
 
 	@AfterMethod
 	public void teardown() {
+		System.out.println("Inside after method of BaseClass");
+		extReport.endTest(extTest);
 		driver.quit();
 	}
-	
+
+	@AfterTest
 	public void dataTearDown() throws IOException {
+		System.out.println("Inside after test of BaseClass");
+
 		wbook.close();
+		extReport.flush();
+		extReport.close();
 	}
 }
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,9 +1,14 @@
 package tests;
 
+import java.lang.reflect.Method;
+
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import pages.LoginPage;
 
@@ -15,25 +20,47 @@ public class LoginPageTest extends BaseClass {
 	LoginPage loginPage;
 
 	@BeforeMethod
-	public void initialize() {
+	public void initialize(Method method) {
 		loginPage = new LoginPage();
+		System.out.println("Inside before method of LoginPageTest");
+		// extReport.startTest(method.getName());
 	}
 
 	@Test
 	public void ValidLoginTest() {
+
+		System.out.println("Inside ValidLoginTest");
+		extTest.log(LogStatus.PASS, "Reading valid username from excel sheet");
+
 		userName = valid_data_sheet.getRow(1).getCell(0).getStringCellValue();
+		extTest.log(LogStatus.PASS, userName);
+		extTest.log(LogStatus.PASS, "Reading valid password from excel sheet");
 		pwd = valid_data_sheet.getRow(1).getCell(1).getStringCellValue();
+		extTest.log(LogStatus.PASS, pwd);
+		extTest.log(LogStatus.PASS, "Valid Login to the application");
 		loginPage.login(userName, pwd);
 		String expUrl = "https://www.saucedemo.com/inventory.html";
 		Assert.assertEquals(loginPage.getCurrentUrl(), expUrl, "Expected URL and Actual URL are NOT EQUAL");
+
 	}
 
 	@Test
 	public void InvalidLoginTest() {
+		extTest.log(LogStatus.PASS, "Reading invalid username from excel sheet");
 		userName = invalid_data_sheet.getRow(1).getCell(0).getStringCellValue();
+		extTest.log(LogStatus.PASS, userName);
+		extTest.log(LogStatus.PASS, "Reading invalid password from excel sheet");
 		pwd = invalid_data_sheet.getRow(1).getCell(1).getStringCellValue();
+		extTest.log(LogStatus.PASS, pwd);
+		extTest.log(LogStatus.PASS, "Valid Login to the application");
 		loginPage.login(userName, pwd);
 		String expMsg = "Epic sadface: Username and password do not match any user in this service";
 		Assert.assertEquals(loginPage.getErrorMsg(), expMsg, "Expected and Actual are NOT EQUAL");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Inside after method of LoginPageTest");
+		// extReport.endTest(extTest);
 	}
 }
